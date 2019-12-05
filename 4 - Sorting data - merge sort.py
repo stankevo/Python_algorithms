7e7512846522ab350eb08bab0ab3ac408060f401
stankevo1 <elena.st463@gmail.com>
2019-12-05 17:36:41 +0100

    4. Sorting data - merge sort - start2

--- 4 - Sorting data - merge sort.py
+++ 4 - Sorting data - merge sort.py
@@ -1,22 +1,29 @@
-def split_array(arr):
-    res = []
+#def split_array(arr):
+#    res = []
+#    if len(arr) == 1:
+#        res.append = arr
+#        print(res)
+#        return (res)
+#    else:
+#        l = len(arr) // 2
+#        arr1 = arr[0:l]
+#        arr2 = arr[l:len(arr)]
+#        split_array(arr1)
+#        split_array(arr2)
+
+
+#        res.append(arr1)
+#        res.append(arr2)
+#        print(res)
+#        return res
+
+
+def merge_sort(arr):
     if len(arr) == 1:
-        res.append = arr
-        print(res)
-        return (res)
+        print('Done!')
     else:
-        l = len(arr) // 2
-        arr1 = arr[0:l]
-        arr2 = arr[l:len(arr)]
-        split_array(arr1)
-        split_array(arr2)
+        for i in range(0, len(arr), 2):
+            print(arr[i], arr[i+1])
 
-
-        res.append(arr1)
-        res.append(arr2)
-        print(res)
-        return res
-
-
-a = [1]
-split_array(a)
+            temp = []
+            for j <= max(len(arr)

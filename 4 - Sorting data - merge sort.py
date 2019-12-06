5eb37ce3f7d70c8ad0431a9dcf25d073dec0be02
stankevo1 <elena.st463@gmail.com>
2019-12-06 14:41:55 +0100

    4. Sorting data - merge sort - split and merge fuctions

--- 4 - Sorting data - merge sort.py
+++ 4 - Sorting data - merge sort.py
@@ -1,72 +1,82 @@
-#def split_array(arr):
-#    res = []
-#    if len(arr) == 1:
-#        res.append = arr
-#        print(res)
-#        return (res)
-#    else:
-#        l = len(arr) // 2
-#        arr1 = arr[0:l]
-#        split_array(arr1)
-#        split_array(arr2)
-#
-#
-#
- #       res.append(arr1)
-  #      res.append(arr2)
-   #     print(res)
-    #    return res
+def split_array(arr):
+   res = []
+   if len(arr) == 1:
+        res[0] = arr
+   else:
+        l = len(arr) // 2
+        arr1 = arr[0:l]
+        arr2 = arr[l:len(arr)]
+        res.append(arr1)
+        res.append(arr2)
+   return res
 
+#a = [1,2,3,4,5]
+#print(a)
+#print(split_array(a))
 
-#a = [1]
-#split_array(a)
-
-
-def mergeSort(arr):
-    if len(arr) == 1:
-        print('Done!')
-    else:
-        a1 = arr[0]
-        a2 = arr[1]
-        i = 0
-        j = 0
-        temp = []
-        while i < len(a1):
-            while j < len(a2):
-                if i <= j:
-                    temp.append(a1[i])
-                    i += 1
-                else:
-                    temp.append(a2[j])
-                    j += 1
-        print(temp)
-
-arr = [[2,4], [1,3,5]]
 def merge(a1, a2):
-    i = a1.pop()
-    j = a2.pop()
-    temp = []
-    while (len(a1) != 0) or (len(a2) != 0):
-        if i <= j:
-            temp.append(i)
-            i = a1.pop()
+    res = []
+    i = 0
+    j = 0
+    while i < len(a1) or j < len(a2):
+        if i >= len(a1):
+            res.append(a2[j])
+            j += 1
+        elif j >= len(a2):
+            res.append(a1[i])
+            i += 1
+        elif a1[i] <= a2[j]:
+            res.append(a1[i])
+            i += 1
         else:
-            temp.append(j)
-            j = a2.pop()
-    e1 = a1.pop()
-    e2 = a2.pop()
-    if e1 != None:
-        temp.append(e1)
-    if e2 != None:
-        temp.append(e2)
-    print(temp)
-
-
-merge([2,4], [1,3,5])
-    
+            res.append(a2[j])
+            j += 1
+    return res
 
 
 
-        
 
 
+#    i = a1.pop()
+#    j = a2.pop()
+#    while (i != None and j != None):
+#        if i == None: 
+#            res.append(j)
+#            if len(a2) != 0: j = a2.pop()
+#            else: j = None
+#        elif j == None: 
+#            res.append(i)
+#            if len(a1) != 0: i = a1.pop()
+#            else: i = None
+#        elif i <= j:
+#          res.append(i)
+#          if len(a1) != 0: i = a1.pop()
+#          else: i = None
+#        else:
+#         res.append(j)
+#         if len(a2) != 0: j = a2.pop()
+#         else: j = None
+#    return res
+
+a1 = [1,3,5]
+a2 = [2,3,6,7]
+print(a1)
+print(a2)
+r = merge(a1, a2)
+print(r)
+
+
+
+
+#def merge_sort(arr):
+#    if len(arr) == 1:
+#        print('Done!')
+#   else:
+#        for i in range(0, len(arr), 2):
+#            a1 = arr[i]
+#            a2 = arr[i+1]
+#            print(arr[i], arr[i+1])
+#            for j in range(min(len(a1), len(a2))):
+#                temp = []
+#                temp.append(min(a1[j],a2[j])
+#            for j <= max(len(arr)
